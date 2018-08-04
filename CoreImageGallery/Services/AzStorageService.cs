18b4cd10844405efef7c32a75c09f1e1f720b681
Mohit Chhabra <Mohitchhabra@techie.com>
2018-08-04 14:39:39 +0530

    Add project files.

---
+++ CoreImageGallery/Services/AzStorageService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using ImageGallery.Models;
+using ImageGallery.Model;
+using CoreImageGallery.Data;
+
+namespace CoreImageGallery.Services
+{
+    public class AzStorageService : IStorageService
+    {
+        private static bool ResourcesInitialized { get; set; } = false;
+
+        private const string ImagePrefix = "img_";
+        private readonly CloudStorageAccount _account;
+        private readonly CloudBlobClient _client;
+        private readonly string _connectionString;
+        private CloudBlobContainer _uploadContainer;
+        private CloudBlobContainer _publicContainer;
+
+        private ApplicationDbContext _dbContext;
+
+        public AzStorageService(IConfiguration config, ApplicationDbContext dbContext)
+        {
+            _connectionString = "DefaultEndpointsProtocol=https;AccountName=bdotnetdemo;AccountKey=hWhCflbbHB27iXei9BlyYgmLjN0TwfeHAiu/vb3a10u/Nv+2d/lCnEhHVV3rj4/u96ajyiJVUc2wtD5i8KBovA==;EndpointSuffix=core.windows.net";
+            _account = CloudStorageAccount.Parse(_connectionString);
+            _client = _account.CreateCloudBlobClient();
+            _uploadContainer = _client.GetContainerReference(Config.UploadContainer);
+            _publicContainer = _client.GetContainerReference(Config.WatermarkedContainer);
+
+            _dbContext = dbContext;
+        }
+
+        public async Task<UploadedImage> AddImageAsync(Stream stream, string originalName, string userName)
+        {
+            await InitializeResourcesAsync();
+
+            string uploadId = Guid.NewGuid().ToString();
+            string fileExtension = originalName.Substring(originalName.LastIndexOf('.'));
+            string fileName = ImagePrefix + uploadId + fileExtension;
+            string userHash = userName.GetHashCode().ToString();
+
+            var imageBlob = _uploadContainer.GetBlockBlobReference(fileName);
+            await imageBlob.UploadFromStreamAsync(stream);
+
+            var img = new UploadedImage
+            {
+                Id = uploadId,
+                FileName = fileName,
+                ImagePath = imageBlob.Uri.ToString(),
+                UploadTime = DateTime.Now,
+                UserHash = userHash
+            };
+
+            await _dbContext.Images.AddAsync(img);
+            await _dbContext.SaveChangesAsync();
+
+            return img;
+        }
+
+
+        public async Task InitializeResourcesAsync()
+        {
+            if (!ResourcesInitialized)
+            {
+                //first Azure Storage resources
+                await _publicContainer.CreateIfNotExistsAsync();
+                await _uploadContainer.CreateIfNotExistsAsync();
+
+                var permissions = await _publicContainer.GetPermissionsAsync();
+                if (permissions.PublicAccess == BlobContainerPublicAccessType.Off || permissions.PublicAccess == BlobContainerPublicAccessType.Unknown)
+                {
+                    // If blob isn't public, we can't directly link to the pictures
+                    await _publicContainer.SetPermissionsAsync(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Blob });
+                }
+
+                ResourcesInitialized = true;
+            }
+
+        }
+
+        public async Task<IEnumerable<UploadedImage>> GetImagesAsync()
+        {
+            await InitializeResourcesAsync();
+
+            var imageList = new List<UploadedImage>();
+            var token = new BlobContinuationToken();
+            var blobList = await _publicContainer.ListBlobsSegmentedAsync(ImagePrefix, true, BlobListingDetails.All, 100, token, null, null);
+
+            foreach (var blob in blobList.Results)
+            {
+                var image = new UploadedImage
+                {
+                    ImagePath = blob.Uri.ToString()
+                };
+
+                imageList.Add(image);
+            }
+
+            return imageList;
+        }
+    }
+}
---
+++ ImageGallery.AzFunctions/WaterMarker.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Watermarker
+{
+    public class WaterMarker
+    {
+        public static void WriteWatermark(string watermarkContent, Stream originalImageStrm, Stream newImageStrm)
+        {
+            originalImageStrm.Position = 0;
+
+            using (Image inputImage = Image.FromStream(originalImageStrm, true))
+            using (Graphics graphic = Graphics.FromImage(inputImage))
+            {
+                Font font = new Font("Georgia", 12, FontStyle.Bold);
+                SizeF textSize = graphic.MeasureString(watermarkContent, font);
+
+                float xCenterOfImg = (inputImage.Width / 2);
+                float yPosFromBottom = (int)(inputImage.Height * 0.90) - (textSize.Height / 2);
+
+                graphic.SmoothingMode = SmoothingMode.HighQuality;
+                graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                graphic.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+
+                StringFormat StrFormat = new StringFormat
+                {
+                    Alignment = StringAlignment.Center
+                };
+
+                SolidBrush semiTransBrush2 = new SolidBrush(Color.FromArgb(153, 0, 0, 0));
+                graphic.DrawString(watermarkContent, font, semiTransBrush2, xCenterOfImg + 1, yPosFromBottom + 1, StrFormat);
+
+                SolidBrush semiTransBrush = new SolidBrush(Color.FromArgb(153, 255, 255, 255));
+                graphic.DrawString(watermarkContent, font, semiTransBrush, xCenterOfImg, yPosFromBottom, StrFormat);
+                graphic.Flush();
+
+                //Saving the image contents directly into the output stream doesn't work in V2 functions
+                //workaround is to write to a memory stream first and copy to the output stream
+                Stream tempStrm = new MemoryStream();
+                inputImage.Save(tempStrm, inputImage.RawFormat);
+                tempStrm.Seek(0, SeekOrigin.Begin);
+                tempStrm.CopyTo(newImageStrm);
+            }
+        }
+    }
+}
